0e95bb14796b46f93d35ea167b996fbfcf58cfac
4ourroro <sujinthornfour@gmail.com>
2025-10-26 13:55:10 +0700

    add image in form6

--- Arthistoryproject/Form6.cs
+++ Arthistoryproject/Form6.cs
@@ -16,5 +16,10 @@ namespace Arthistoryproject
         {
             InitializeComponent();
         }
+
+        private void Form6_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
